6c7bddd06cf97226661da81eeea3e831e04e393e
Mark-Zwaving <markzwaving@gmail.com>
2022-05-04 08:17:08 +0200

    Added make function

--- auto/dayvalues.py
+++ auto/dayvalues.py
@@ -29,6 +29,17 @@ lst_stations = [
     stations.wmo_to_station('380')
 ]
 
+# Option list to make dayvalues
+options = {
+    'title': 'dayvalues',          # Title for verbose options
+    'lst-stations': lst_stations,  # Selected stations
+    'period': '*',                 # Period to make dayvalues
+    'file-type': 'html',           # Output type file
+    'write': 'rewrite',            # Rewrite 'rewrite' all or just just 'add'
+    'lst-sel-cells': cfg.lst_cells_dayvalues,  # Entities for the day. Use all.
+    'download': True               # Download data first
+}
+
 # No need to update it all (everytime)
 # Period for last X days
 days = 31 # days to go back
@@ -36,17 +47,20 @@ end_ymd = ymd.yyyymmdd_now()
 start_ymd = ymd.yyyymmdd_minus_day(end_ymd, days)
 per_last_X_days = f'{start_ymd}-{end_ymd}'
 
-# Option list to make dayvalues
-options = {
-    'title': 'dayvalues',          # Title for verbose options
-    'lst-stations': lst_stations,  # Selected stations
-    'period': per_last_X_days,     # Period last X days
-    # 'period': '*',                 # All times
-    'file-type': 'html',           # html files
-    'write': 'rewrite',            # Rewrite 'rewrite' all or just just 'add'
-    'lst-sel-cells': cfg.lst_cells_dayvalues,  # Entities for the day. Use all.
-    'download': True               # Download data first
-}
+# Make dayvalues
+def make(lst_stations, period):
+    # Option list to make dayvalues
+    options = {
+        'title': 'dayvalues',          # Title for verbose options
+        'lst-stations': lst_stations,  # Selected stations
+        'period': period,              # All times
+        'file-type': 'html',           # html files
+        'write': 'rewrite',            # Rewrite 'rewrite' all or just just 'add'
+        'lst-sel-cells': cfg.lst_cells_dayvalues,  # Entities for the day. Use all.
+        'download': True               # Download data first
+    }
+    dayvalues.calculate(options)
 
 if __name__ == '__main__':
-    dayvalues.calculate(options)
+    # Example all days for lst stations
+    make(lst_stations, '*')
