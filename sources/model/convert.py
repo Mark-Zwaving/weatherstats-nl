f41d17f548060cf4afd045393e87cdf6db0920e8
mark <mark@mzg-debian-work>
2023-03-14 19:15:49 +0100

    Added fn to_lst()

--- sources/model/convert.py
+++ sources/model/convert.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
-'''Library contains functions for converting data'''
-__author__     =  "Mark Zwaving"
-__email__      =  "markzwaving@gmail.com"
-__copyright__  =  "Copyright (C) Mark Zwaving. All rights reserved."
-__license__    =  "GNU Lesser General Public License (LGPL)"
-__version__    =  "0.4.4"
-__maintainer__ =  "Mark Zwaving"
-__status__     =  "Development"
+'''Library contains functions for converting elements'''
+__author__     = 'Mark Zwaving'
+__email__      = 'markzwaving@gmail.com'
+__copyright__  = 'Copyright (C) Mark Zwaving. All rights reserved.'
+__license__    = 'GNU General Public License version 3 - GPLv3'
+__version__    = '0.4.6'
+__maintainer__ = 'Mark Zwaving'
+__status__     = 'Development'
 
 import config as cfg
-import sources.view.text as text
+import sources.view.console as cnsl
+
+# Convert types
+lst_to_s = lambda lst, sep=', ': sep.join(lst)
+fl_to_s = lambda f: str(int(round(f)))
+
+def to_lst(s):
+    lst = []
+    if isinstance(s, str):  
+        lst = [s]
+    elif isinstance(s, list): 
+        lst = s
+    elif isinstance(s, tuple): 
+        lst = list(s)
+
+    return lst
 
 # Convert temperatures
 celsius_to_fahrenheit = lambda c:   float(c) * 1.8 + 32.0
@@ -36,7 +51,54 @@ yard_to_cm     = lambda y:   float(y) * 91.44
 cm_to_yard     = lambda c:   float(c) / 91.44
 
 # Default dpi in matplotlib is 100. See cfg.py
-pixel_to_inch  = lambda p: float(p) / float(cfg.plot_dpi)
+pixel_to_inch = lambda pixel, dpi=100.0: float(pixel) / float(dpi)
+
+bytes_ascii_to_str = lambda b: bytes_to_str(b, 'ascii', 'ignore')
+bytes_utf8_to_str  = lambda b: bytes_to_str(b, 'utf-8', 'ignore')
+
+def s_to_bytes( s, charset, errors ):
+    try:
+        b = s.encode(encoding=charset, errors=errors)
+    except Exception as e:
+        cnsl.log(f'Fail convert to bytes with charset {charset}\nError {e}', True)
+    else:
+        return b
+    return s
+
+def bytes_to_s( b, charset, errors ):
+    try:
+        s =  b.decode(encoding=charset, errors=errors)
+    except Exception as e:
+        cnsl.log(f'Fail convert to string with charset {charset}.\nError:{e}', True)
+    else:
+        return s
+    return b
+
+def b_ascii_to_s( b ):
+    s = bytes_to_s(b, 'ascii', 'ignore')
+    return s
+
+def b_utf8_to_s( b ):
+    s = bytes_to_s(b, 'utf-8', 'ignore')
+    return s
+
+def str_to_bytes( s, charset, errors ):
+    try:
+        b = s.encode(encoding=charset, errors=errors)
+    except Exception as e:
+        cnsl.log(f'Error convert string to bytes charset {charset}\n{e}', cfg.error)
+    else:
+        return b
+    return s
+
+def bytes_to_str( b, charset, errors ):
+    try:
+        s =  b.decode(encoding=charset, errors=errors)
+    except Exception as e:
+        cnsl.log(f'Error convert bytes to string charset {charset}\n{e}', cfg.error)
+    else:
+        return s
+    return b
 
 def ms_to_bft( ms ):
     i = int(ms)
@@ -56,85 +118,72 @@ def ms_to_bft( ms ):
 
 def octa_to_txt(octa):
     i = int(octa)
-    if   0 == i: return text.tr('Onbewolkt')
-    elif 1 == i: return text.tr('Vrijwel onbewolkt')
-    elif 2 == i: return text.tr('Licht bewolkt')
-    elif 3 == i: return text.tr('Half bewolkt')
-    elif 4 == i: return text.tr('Half bewolkt')
-    elif 5 == i: return text.tr('Half tot zwaar bewolkt')
-    elif 6 == i: return text.tr('Zwaar bewolkt')
-    elif 7 == i: return text.tr('Vrijwel geheel bewolkt')
-    elif 8 == i: return text.tr('Geheel bewolkt')
-    elif 9 == i: return text.tr('Bovenlucht onzichtbaar')
+    if   0 == i: return 'Onbewolkt'
+    elif 1 == i: return 'Vrijwel onbewolkt'
+    elif 2 == i: return 'Licht bewolkt'
+    elif 3 == i: return 'Half bewolkt'
+    elif 4 == i: return 'Half bewolkt'
+    elif 5 == i: return 'Half tot zwaar bewolkt'
+    elif 6 == i: return 'Zwaar bewolkt'
+    elif 7 == i: return 'Vrijwel geheel bewolkt'
+    elif 8 == i: return 'Geheel bewolkt'
+    elif 9 == i: return 'Bovenlucht onzichtbaar'
     else: return ''
 
 def deg_to_txt(deg):
     i = int(deg)
-    if   i ==  0:  return text.tr('stil')
-    elif i  < 22:  return text.tr('NOORD')
-    elif i  < 30:  return text.tr('NOORDNOORDOOST')
-    elif i  < 67:  return text.tr('NOORDOOST')
-    elif i  < 112: return text.tr('OOST')
-    elif i  < 157: return text.tr('ZUIDOOST')
-    elif i  < 202: return text.tr('ZUID')
-    elif i  < 247: return text.tr('ZUIDWEST')
-    elif i  < 292: return text.tr('WEST')
-    elif i  < 337: return text.tr('NOORDWEST')
-    elif i  < 360: return text.tr('NOORD')
-    elif i == 990: return text.tr('veranderlijk')
+    if   i ==  0:  return 'quiet'
+    elif i  < 22:  return 'north'
+    elif i  < 30:  return 'north-north-east'
+    elif i  < 67:  return 'north-east'
+    elif i  < 112: return 'east'
+    elif i  < 157: return 'south-east'
+    elif i  < 202: return 'south'
+    elif i  < 247: return 'south-west'
+    elif i  < 292: return 'west'
+    elif i  < 337: return 'north-west'
+    elif i  < 360: return 'north'
+    elif i == 990: return 'variable'
     else: return ''
 
-def ms_to_txt( ms ):
+def ms_to_txt( ms ): # TODO
     i = int(ms)
-    if   i <   3: return text.tr('windstil')
-    elif i <  16: return text.tr('zwakke wind')
-    elif i <  34: return text.tr('zwakke wind')
-    elif i <  55: return text.tr('matige wind')
-    elif i <  80: return text.tr('matige wind')
-    elif i < 108: return text.tr('vrij krachtige wind')
-    elif i < 139: return text.tr('krachtige wind')
-    elif i < 172: return text.tr('harde wind')
-    elif i < 208: return text.tr('stormachtige wind')
-    elif i < 245: return text.tr('storm')
-    elif i < 285: return text.tr('zware storm')
-    elif i < 327: return text.tr('zeer zware storm')
-    else: return text.tr('orkaan')
+    if   i <   3: return 'windstil'
+    elif i <  16: return 'zwakke wind'
+    elif i <  34: return 'zwakke wind'
+    elif i <  55: return 'matige wind'
+    elif i <  80: return 'matige wind'
+    elif i < 108: return 'vrij krachtige wind'
+    elif i < 139: return 'krachtige wind'
+    elif i < 172: return 'harde wind'
+    elif i < 208: return 'stormachtige wind'
+    elif i < 245: return 'storm'
+    elif i < 285: return 'zware storm'
+    elif i < 327: return 'zeer zware storm'
+    else: return 'orkaan'
 
 def vvn_to_txt( vvn ):
     i = int(vvn)
-    # 1:100-200 m ... 49:4900-5000 m
-    if vvn <= 49:
-        return f'{i*100} - {(i+1)*100}' + text.tr('meter')
-    # 50:5-6 km
-    elif vvn == 50:
-        return '5 - 6' + text.tr('km')
-    # 56:6-7 km ... 79:29-30 km
-    elif vvn <= 79:
-        return f'{i-50} - {i-49}' + text.tr('km')
-    #  80:30-35 km ... 87:65-70 km
-    elif vvn  < 89:
-        return '{(i-80)*5+30} - {(i-79)*5+30}' + text.tr('km')
-    # 89: >70 km
-    elif vvn == 89:
-        return '< 70' + text.tr('km')
-    else:
-        return ''
+    if   i <= 49: return f'{i*100} - {(i+1)*100} meter' # 1:100-200 m ... 49:4900-5000 m
+    elif i == 50: return '5 - 6 km' # 50:5-6 km
+    elif i <= 79: return f'{i-50} - {i-49} km' # 56:6-7 km ... 79:29-30 km
+    elif i  < 89: return f'{(i-80)*5+30} - {(i-79)*5+30} km' #  80:30-35 km ... 87:65-70 km
+    elif i == 89: return '< 70 km' # 89: >70 km
+    return vvn
 
 def timespan1hour(h):
     i = int(h)
-    return f'{i-1} - {i}' + text.tr('hour')
+    return f'{i-1} - {i} hour'
 
 def timespan6hour(u):
     i = int(u)
-    if   i ==  6:
-        s  = '0 - 6'
-    elif i == 12:
-        s  = '6 - 12'
-    elif i == 18:
-        s  = '12 - 18'
-    elif i == 24:
-        s  = '18 - 24'
-    else:
-        s  = ''
+    if   i ==  6: s = '0 - 6'
+    elif i == 12: s = '6 - 12'
+    elif i == 18: s = '12 - 18'
+    elif i == 24: s = '18 - 24'
+    else: s = u
+    return f'{s} hour'
 
-    return s + text.tr('hour')
+def mp4_to_jpgs( mp4 ):
+    #
+    pass
