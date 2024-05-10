cd7bda45c9349b34655253b3d78592743f56d17e
mark <mark@debian>
2024-05-10 14:53:09 +0200

    TODO. Updated references to moved and rewritten libraries

--- extra/month-extremes.py
+++ extra/month-extremes.py
@@ -16,8 +16,8 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirna
 
 # Import weatherstats libraries
 import config as cfg
-import sources.model.weather_stations as weather_stations
-import sources.model.stats_tables as stats_tables
+import sources.model.stations as stations
+import sources.view.table as table
 import sources.model.ymd as ymd 
 
 # See all the files made
@@ -34,7 +34,7 @@ options = {
     # Title for table Januari 01 
     'title': f'month extremes {mm}',
     # List with the stations to add in the table 
-    'lst-stations': weather_stations.lst,
+    'lst-stations': stations.lst,
     # Global period: * for all
     'period': '*',
     # This is the period string for the calculation of one day
@@ -58,7 +58,7 @@ options = {
 def extremes_in_a_month(mm):
     options = {
         'title': f'month extremes {mm}',
-        'lst-stations': weather_stations.lst,
+        'lst-stations': stations.lst,
         'period': '*',                    
         'period-2': f'****{mm}*', # TODO              
         'lst-sel-cells': cfg.lst_cells_my_extremes, 
@@ -67,7 +67,7 @@ def extremes_in_a_month(mm):
         'period-cmp': '',   
         's4d-query': '', 
     }
-    stats_tables.calculate(options)
+    table.calculate(options)
 
 
 ################################################################################
