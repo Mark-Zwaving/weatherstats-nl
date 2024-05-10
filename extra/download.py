6596b77af93b9e464ded2bfcb478c253c9f28559
mark <mark@debian>
2024-05-10 14:54:20 +0200

    TODO. Updated references to moved and rewritten libraries

--- extra/download.py
+++ extra/download.py
@@ -4,7 +4,7 @@ __author__ = 'Mark Zwaving'
 __email__ = 'markzwaving@gmail.com'
 __copyright__ = 'Copyright (C) Mark Zwaving. All rights reserved.'
 __license__ = 'GNU Lesser General Public License (LGPL)'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __maintainer__ = 'Mark Zwaving'
 __status__ = 'Development'
 # To get access to all the code from weathersstats,
@@ -15,23 +15,23 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirna
 # Import weatherstats libraries
 import sources.model.ymd as ymd 
 import sources.view.console as cnsl
-import sources.model.daydata as daydata
-import sources.model.weather_stations as weather_stations
+import sources.model.dayvalues.data as data
+import sources.model.stations as stations
 
 ################################################################################
 # Stations lists
 
 # List with stations for download
 lst_sel_stations = [
-    weather_stations.wmo_to_station('260'),
-    weather_stations.wmo_to_station('280'),
-    weather_stations.wmo_to_station('235'),
-    weather_stations.wmo_to_station('310'),
-    weather_stations.wmo_to_station('380')
+    stations.wmo_to_station('260'),
+    stations.wmo_to_station('280'),
+    stations.wmo_to_station('235'),
+    stations.wmo_to_station('310'),
+    stations.wmo_to_station('380')
 ]
 
 # All available weather stations
-lst_all_stations = weather_stations.lst 
+lst_all_stations = stations.lst 
 
 
 ################################################################################
@@ -39,7 +39,7 @@ lst_all_stations = weather_stations.lst
 
 def download(lst):
     cnsl.log(f'[{ymd.now()}] Start download', True)
-    daydata.process_lst(lst)
+    data.process_lst(lst)
     cnsl.log(f'[{ymd.now()}] End download', True)
 
 
